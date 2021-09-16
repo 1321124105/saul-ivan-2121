53f1c685e29979a2f1588c77cf68f8d621811708
1321124105 <saul_1321124105@uptecamac.edu.mx>
2021-09-15 23:35:44 -0500

    avance 1

---
+++ actividad.js
@@ -0,0 +1,16 @@
+var plumon1="rojo";
+var plumon="verde"; 
+var amigo="axel";
+switch(persona){
+   case "ivan";
+        console.log("ivan compro un plumon rojo");
+         break;
+
+}
+switch(plumon){
+     case"verde":
+        console.log("mi nombre es "+amigo,"y me gusta el plumon"+plumon1);
+         break
+         
+
+}
---
+++ pecadito.js
@@ -0,0 +1,14 @@
+var transporte = "caminando"
+
+console.log("paso 1: salir  a la entrada principal de mi casa ");
+if(transporte=="caminando") {
+    console.log("paso 2 : ir a la tortilleria ");
+
+}else{
+    console.log("paso 2: pedir un kilo de tortillas");
+}
+
+console.log ("paso 3: pagar el kilo de tortillas");
+console.log("paso 4: recibir el cambio");
+console.log("paso 5: regresar a mi casa");
+
