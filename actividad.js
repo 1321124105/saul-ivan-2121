e44382d08ccb03ccd6a6d1032878b781d7396a31
1321124105 <saul_1321124105@uptecamac.edu.mx>
2021-09-16 21:16:37 -0500

    terminado

--- actividad.js
+++ actividad.js
@@ -57,7 +57,7 @@ var amigo="jose";
      switch(1){
         case"ivan":
         console.log("ivan va "+salir);
-         break;
+        break;
  
 }
       
@@ -87,32 +87,125 @@ var amigo="jose";
 var tiempo ="3";
 var clima ="frio";
 switch(tiempo){
-     case"3":
-     if(clima =="calor") {
+    case"3":
+    if(clima =="calor") {
         console.log("si esta haciendo frio");
         
-     } else {
+    } else {
 
         console.log("esta haciendo calor");
-     }
+    }
      break;
     }
 
 
 //ejercicio2
 
-var 
+var jugar="canicas";
+var hobbi="tocar guitarra";
+switch(jugar){
+    case"canicas":
+    if(hobbi=="se rompio una cuerda"){
+         console.log("si se rompio una cuerda");
 
+    } else{
+        
+        console.log("jugare canicas");
 
+    }
+     break;
+    }
 
+//ejercicio3 
 
+var futbol="gano cruz azul";
+var peligro="se lesiono jugador"
+switch(futbol){
+    case"se callo":
+    if(futbol=="no gano cruz azul"){
+       console.log("si gano cruz azul");
+       
+
+    } else{
+ 
+        console.log("no gano cruz azul");
+
+    }
+    break;
+    }
 
      
-      
-
-
+//ejecicio 4
  
-   
+  var agua="2";
+  var sabor="tamarindo";
+  switch(sabor){
+       case"2":
+       if(sabor=="piña"){
+        console.log(" el agua es de tamarindo");
+
+    } else{
+       console.log("no el agua es de piña");
+
+    }
+    break;
+    }
+
+//ejercicio 5 
+
+var limpio="3";
+var  baño="me bañe hoy";
+switch(baño){
+    case"3":
+    if(baño=="no me bañe hoy"){
+        console.log("si me bañe hoy");
+
+
+    } else{
+      console.log("no me bañe hoy");
+
+    }
+    break;
+    }
+
+//condiciones de if y else
+if (b==0.9){
+    console.log("el  numero decimal si es igual");
+    
+}else{
+    console.log("el numero decimal es diferente");
+}
+
+//ejercicio 2
+if( c ==4>3){
+   console.log(" 4 es mayor que 3");
+
+}else{
+  console.log ("4 no es mayor que 3"); 
+}
+//ejercicio 3
+if(a==14){
+   console.log("el numero es 14");
+
+}else{
+    console.log("el numero no es 14");
+
+}
+//ejercicio 4
+if(c==1<3){
+    console.log("1 es menor que 3");
+
+}else{
+    console.log("1 es mayo que 3");
+}
+//ejercicio 5
+if(h=="contabilidad"){
+   console.log("contabilidad no es lo mismo que softaware");
+
+}else{
+    console.log("contabilidad si es lo mismo que softaware")
+}
+
 
 
 
