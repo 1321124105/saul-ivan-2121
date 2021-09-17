4b06bf6f60b84f7e0774aa327a1949cf9843534e
1321124105 <saul_1321124105@uptecamac.edu.mx>
2021-09-16 21:00:44 -0500

    terminado

--- actividad.js
+++ actividad.js
@@ -1,16 +1,123 @@
+//condiciones de switch
 var plumon1="rojo";
 var plumon="verde"; 
 var amigo="axel";
-switch(persona){
-   case "ivan";
+switch (1) {
+   case "ivan":
         console.log("ivan compro un plumon rojo");
-         break;
+        break;
 
 }
-switch(plumon){
-     case"verde":
+switch(plumon) {
+     case "verde":
         console.log("mi nombre es "+amigo,"y me gusta el plumon"+plumon1);
-         break
+        break;
          
+}
+// ejercicio 2 
+var refresco="coca cola";
+var alcohol="jack Daniel´s";
+var refresco2="sprite"
+var amigo="jose";
+ switch(3){
+  case "sergio":
+      console.log("sergio compro un jack Daniel´s");
+       break;
+      
+}
+ switch(amigo) {
+     case "coca cola":
+      console.log("jose convino "+refresco2,"con"+alcohol);
+       break;
+    
+}
+
+ //ejercicio 3
+  var jugar="xbox";
+  var jugar2="playstation";
+  var amigo="axel";
+    switch(4){
+     case"enrique":
+      console.log("axel juega xbox con enrique");
+       break;
 
 }
+
+ switch(amigo) {
+    case"playstation":
+      console.log("axel rompio el"+jugar);
+       break;
+
+}
+
+ //ejercicio 4 
+    var salir="a comer";
+    var quedarse="a estudiar";
+    var visita="angel";
+     switch(1){
+        case"ivan":
+        console.log("ivan va "+salir);
+         break;
+ 
+}
+      
+     switch(visita){
+       case"visita":
+       console.log("saul va"+quedarse);
+        break;
+
+
+
+}
+
+   //ejercicio 5 
+    var decorar="color azul";
+    var persona="juan";
+    var azotea="recoguer";
+
+    switch(3){
+      case"juan":
+      console.log("ivan ayuda a"+persona,"a "+azotea,"la casa" );
+       
+
+}
+
+//condiciones de switch e if
+ 
+var tiempo ="3";
+var clima ="frio";
+switch(tiempo){
+     case"3":
+     if(clima =="calor") {
+        console.log("si esta haciendo frio");
+        
+     } else {
+
+        console.log("esta haciendo calor");
+     }
+     break;
+    }
+
+
+//ejercicio2
+
+var 
+
+
+
+
+
+     
+      
+
+
+ 
+   
+
+
+
+    
+    
+    
+
+ 
